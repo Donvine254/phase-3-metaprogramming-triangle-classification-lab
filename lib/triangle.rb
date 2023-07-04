64c0679b550014962c3054a2b9df2cda57fdcb97
donvine mugendi <donvinemugendi@gmail.com>
2023-07-04 23:44:49 +0300

    completed lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :base, :right, :left
+
+  def initialize(base, right, left)
+    @base = base
+    @right = right
+    @left = left
+  end
+
+  def kind
+    invalid_error
+
+    triangle_kind = case
+    when @base == @left && @left == @right then :equilateral
+    when @base == @left || @right == @left || @base == @right then :isosceles
+    when @base != @right && @left != @right && @base != @left then :scalene
+    end
+  end
+
+  def invalid_error
+    if @base <= 0 || @right <= 0 || @left <= 0
+      raise TriangleError
+    elsif @base + @right <= @left || @base + @left <= @right || @left + @right <= @base
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Please specify valid triangle side lengths."
+    end
+  end
 end
